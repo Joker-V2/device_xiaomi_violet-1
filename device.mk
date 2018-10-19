531d0d6b1372557f6ad6e387f2e88fe62b64bf78
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-10-19 16:59:43 +0200

    violet: Build NoCutoutOverlay
    Change-Id: I45edeb3ae20f374cd4510bad83b3a51059a7e743

--- device.mk
+++ device.mk
@@ -237,6 +237,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service
 
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # IFAA manager
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
